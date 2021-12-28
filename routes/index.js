85f22dba7be763679ed5669e73d8e7a0cfcfba42
O Andras <43110418+oandras22@users.noreply.github.com>
2021-12-28 16:33:17 +0100

    v.1.2

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (!res.locals.broken) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express v.1.2' });
   } else {
   	res.status(500).send('Something broke!');
   }
